6f74c2748c146ce3266ed41fd6dbad8ecf94c674
mashiyama <wonderwall0803@gmail.com>
2024-08-16 17:25:07 +0900

    update noindex

--- next-app/pages/_document.tsx
+++ next-app/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="robots" content="noindex" />
           <title>MASHIYAMA | PORTFOLIO</title>
           <meta name="description" content="フロントエンドエンジニアの実績紹介サイトです。" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet" />
